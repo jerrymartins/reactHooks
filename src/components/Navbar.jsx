76adfc5c2068d4f6fdad3e273cda6495290b782e
jerry martins <zeroumbin@gmail.com>
2019-09-29 15:52:20 -0400

    titutlo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ export default function Navbar() {
                         <SideBar/>
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        ReactApp
+                        TRAILIX
                     </Typography>
                     <div>
                         <IconButton
