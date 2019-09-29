67977bf6b9c043b7f812d15b9dfa1a02b51aff81
jerry martins <zeroumbin@gmail.com>
2019-09-29 13:18:38 -0400

    enviroments

--- src/environments/development.js
+++ src/environments/development.js
@@ -1,6 +1,6 @@
 import environment from './base';
 //const baseApi = 'https://nicaf-nodejs.herokuapp.com';
-const baseApi = 'http://dynamoxapi-env-1.3skymcmvag.us-east-1.elasticbeanstalk.com/';
+const baseApi = 'http://dynamoxapi.us-east-1.elasticbeanstalk.com/';
 const env = environment(baseApi);
 export default {
     ...env,
--- src/environments/production.js
+++ src/environments/production.js
@@ -1,6 +1,6 @@
 // src/environments/production.ts
 import environment from './base';
-const baseApi = 'http://dynamoxapi-env-1.3skymcmvag.us-east-1.elasticbeanstalk.com/';
+const baseApi = 'http://dynamoxapi.us-east-1.elasticbeanstalk.com/';
 const env = environment(baseApi);
 export default {
     ...env,
